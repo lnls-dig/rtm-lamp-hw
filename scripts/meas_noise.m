2ba091c3652caae525e195214e37a07399ed51de
Daniel Tavares <daniel.tavares@lnls.br>
2017-12-21 00:52:20 -0200

    meas_noise.m: rewrite of noise analysis
    Main fix: current and deflection noise should be inversely scaled
    with in-amp gain.
    
    In-amp model is more complete now, taking into account R_shunt, R_g
    and input resistance thermal noise, as well as in-amp current noise.

--- scripts/meas_noise.m
+++ scripts/meas_noise.m
@@ -1,5 +1,5 @@
-% Noise analysis to aid on selection instrumentation amplifier for current
-% measurement.
+% Noise analysis to aid on selection instrumentation amplifier (in-amp)
+% for current measurement.
 
 % Copyright (C) 2017 CNPEM
 % Licensed under GNU General Public License v3.0 (GPL)
@@ -7,93 +7,90 @@
 % Author(s): Daniel Tavares (LNLS/DIG) - daniel.tavares@lnls.br
 
 Pmax = 1/8;                     % Maximum power dissipation on shunt resistor [W]
-
-th_FS = 30e-6;                  % Beam deflection full-scale [rad]
-req_Th_noise = 300e-12;         % Required beam deflection noise spectral density [rad/sqrt(Hz)]
+req_th_noise = 300e-12;         % Required beam deflection noise spectral density [rad/sqrt(Hz)]
 
 I_FS = 1;                       % Load current full-scale [A]
-Rs = 50e-3;                     % Shunt resistance for current measurement [ohm]
+th_FS = 30e-6;                 % Beam deflection full-scale [rad]
 
-SNR_adc = 80;                   % ADC SNR [dBFS]
-fs = 1.2e6;                     % ADC sampling rate
-FS_adc = 2;                     % ADC full-scale voltage [V]
+Rs = 15e-3;                     % Shunt resistance for current measurement [ohm]
 
-G_inamp = logspace(-2, 3, 9)';  % In-amp gain
-eni_inamp = 50e-9;              % In-amp input noise spectral density [V/sqrt(Hz)]
-eno_inamp = 45e-9;              % In-amp output noise spectral density [V/sqrt(Hz)]
+% ADC data (based on LTC2320-16 datasheet)
+SNR_adc = 80;                   % SNR [dBFS]
+fs = 1.2e6;                     % sampling rate
+FS_adc = 2;                     % Full-scale voltage [V]
 
-% Voltage noise
-en_inamp = sqrt(G_inamp*eni_inamp.^2 + eno_inamp.^2);
-en_adc = FS_adc*10^(-SNR_adc/20)./sqrt(fs/2);
-en_total = sqrt(en_inamp.^2 + en_adc.^2);
+% In-amp data (based on AD8429 datasheet)
+G = [1; ...                     % Gain
+     5; ...
+     10; ...
+     20; ...
+     50; ...
+     100];
+eni_inamp = [50e-9; ...         % Input voltage noise spectral density [V/sqrt(Hz)]
+             10e-9; ...
+             5.5e-9; ...
+             3e-9; ...
+             2.2e-9; ...
+             1.8e-9];
+eno_inamp = 45e-9;              % Output voltage noise spectral density [V/sqrt(Hz)]
+in_inamp = 2e-12;               % Input current noise spectral density [A/sqrt(Hz)]
 
-% Measured current noise
-in_inamp = en_inamp./Rs;
-in_adc = en_adc./Rs;
-in_total = sqrt(in_inamp.^2 + in_adc.^2);
+% Calculate range slection resistor (Rg)
+Rg = 6e3./(G-1);
 
-% Measured beam deflection noise
-thn_inamp = th_FS./I_FS*in_inamp;
-thn_adc = th_FS./I_FS*in_adc;
-thn_total = sqrt(thn_inamp.^2 + thn_adc.^2);
+% Prevent infinite or negative Rg
+Rg(G <= 1) = 0;
 
-% Thermal noise spectral density at 300 K temperature
-en_R = sqrt(1.6568e-20*Rs);
-in_R = en_R./Rs;
-thn_R = th_FS./I_FS*in_R;
+% Thermal noise spectral density constant at 300 K temperature [V^2/Hz]
+k_therm = 1.6568e-20;
+
+% In-amp input resistance
+R_input = Rs;
+
+% In-amp noise
+noise_rti = sqrt((eno_inamp./G).^2 + eni_inamp.^2 + (R_input*in_inamp).^2 + k_therm*R_input + k_therm*Rg);
+noise = G.*noise_rti;
+imeas_noise = 1./Rs./G.*noise;
+FS_noise = th_FS./I_FS.*imeas_noise;
+
+% ADC noise floor
+noisefloor = FS_adc*10^(-SNR_adc/20)/sqrt(fs/2);
+imeas_noisefloor = 1./Rs./G.*noisefloor;
+FS_noisefloor = th_FS./I_FS*imeas_noisefloor;
 
 % Ratios
 Pp = Rs*I_FS.^2./Pmax;
-FSp = Rs*I_FS./FS_adc;
+FSp = G*Rs*I_FS./FS_adc;
 
-% Show results
-if length(en_inamp) > 1
-    figure;
-    loglog(G_inamp([1 end]), [en_adc en_adc]/1e-9);
-    hold all
-    loglog(G_inamp, [en_inamp en_total]/1e-9);
-    xlabel('Gain');
-    ylabel('nV/\surdHz');
-    grid on;
-    legend('ADC noise floor', 'In-amp', 'Total');
-    
-    figure;
-    loglog(G_inamp([1 end]), [in_adc in_adc]/1e-6);
-    hold all
-    loglog(G_inamp, [in_inamp in_total]/1e-6);
-    xlabel('Gain');
-    ylabel('\muA/\surdHz');
-    grid on;
-    legend('ADC noise floor', 'In-amp', 'Total');
-    
-    figure;
-    loglog(G_inamp([1 end]), [req_Th_noise req_Th_noise]/1e-12, 'LineWidth', 2);
-    hold all
-    loglog(G_inamp([1 end]), [thn_adc thn_adc]/1e-12);    
-    loglog(G_inamp, [thn_inamp thn_total]/1e-12);
-    xlabel('Gain');
-    ylabel('prad/\surdHz');
-    grid on;
-    legend('Specification', 'ADC noise floor', 'In-amp', 'Total');
-else
-    fprintf('\n');
-    fprintf('Voltage noise\n');
-    fprintf('-------------\n');
-    fprintf('In-amp noise: %0.3g nV/sqrt(Hz)\n', en_inamp/1e-9);
-    fprintf('ADC noise floor: %0.3g nV/sqrt(Hz)\n', en_adc/1e-9);
-    fprintf('\n');
-    fprintf('Measured current\n');
-    fprintf('----------------\n');
-    fprintf('In-amp noise: %0.3g uA/sqrt(Hz)\n', in_inamp/1e-6);
-    fprintf('ADC: %0.3g uA/sqrt(Hz)\n', in_adc/1e-6);
-    fprintf('\n');
-    fprintf('Measured beam deflection\n');
-    fprintf('------------------------\n');
-    fprintf('In-amp noise: %0.3g prad/sqrt(Hz)\n', thn_inamp/1e-12);
-    fprintf('ADC noise floor: %0.3g prad/sqrt(Hz)\n', thn_adc/1e-12);
-    fprintf('\n');
-end
+for i=1:length(FSp)
+    if FSp(i) > 1
+        warning(sprintf('Current full-scale exceeds ADC full-scale by %0.3g%% for G = %d', FSp(i)*100, G(i)));
+    end
+end 
 
-fprintf('Dissipated power (percent of power budget): %0.3g%% (must be < 100%%)\n', Pp*100);
-fprintf('ADC full-scale to current full-scale ratio: %0.3g%% (must be < 100%%)\n', FSp*100);
-fprintf('\n');
+% Plot results
+figure;
+loglog(G, [repmat(noisefloor, size(G)) noise]/1e-9, '-o');
+xlabel('Gain');
+ylabel('nV/\surdHz');
+title('Voltage noise');
+grid on;
+legend('ADC noise floor', 'In-amp noise');
+
+figure;
+loglog(G, [imeas_noisefloor imeas_noise]/1e-6, '-o');
+xlabel('Gain');
+ylabel('\muA/\surdHz');
+title('Current measurement noise');
+grid on;
+legend('ADC noise floor', 'In-amp noise');
+
+figure;
+loglog(G([1 end]), [req_th_noise req_th_noise]/1e-12, 'LineWidth', 2);
+hold all
+loglog(G, [FS_noisefloor FS_noise]/1e-12, '-o');
+xlabel('Gain');
+ylabel('prad/\surdHz');
+title('Beam deflection noise');
+grid on;
+legend('Specification', 'ADC noise floor', 'In-amp noise');
