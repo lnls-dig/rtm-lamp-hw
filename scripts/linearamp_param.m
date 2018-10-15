7f27573ef5636c5f6c4e3e4f598062dedc79d68a
Aylons <aylons@aylons.com.br>
2018-10-14 21:34:23 -0300

    Added plots for dependence on supply voltage and cable length

--- scripts/linearamp_param.m
+++ scripts/linearamp_param.m
@@ -1,5 +1,5 @@
 % Linear amplifier parameter setting
-
+ 
 % System parameters
 L_c = 3.32e-3 % Magnet inductance [H]
 I_c = 1.00   % Fullscale DC curent [A]
@@ -23,47 +23,34 @@ Cb_res = 7.98e-3  % Cable resistivity [Ohm/m]
 f_max = 10e3 % Maximum actuator frequency to analyse [Hz]
 
 % Amplifier data, based on OPA559 datasheet and RTM-DAMP design
-Vs = 3.3   % Supply voltage [V]
-Vswp = 0.08 % Full scale current drop voltage,positive rail [V]
+Vs = 3.5   % Supply voltage [V]
+Vswp = 0.08 % Full scale current drop voltage, positive rail [V]
 Vswn = 0.08 % Full scale current drop voltage, negative rail [V]
-V_mid = Vs/2; % Operation center at tied-load bridge [V]
+R_sh = 40e-3 % Shunt resistor value [Ohm]
 
-R_sh = 0.04 % Shunt resistor value [Ohm]
+Vmid = (Vs-Vswp+Vswn)/2;
+Vswing = Vs - Vswp - Vswn;
 
 
+%% Frequency response for standard case
 % Total cable resistance
 R_cb = 2*Cb_length*Cb_res;
 Rt = R_cb + R + R_sh;
 
 % DC operating values
-
 Vomax = Rt.*I;
 
-Vmargin_p = (Vs - Vswp) - (V_mid+Vomax/2); % Lowest margin between
-                                           % OpAmp and load,
-                                           % positive rail
-
-Vmargin_n = (V_mid - Vomax/2) - Vswn;      % Lowest margin between
-                                           % OpAmp and load,
-                                           % negative rail
-
-Vmargin = Vmargin_p + Vmargin_n; %Highest step possible at worst
-                                 %case
+Vmargin = Vswing - Vomax;
 
 % AC operating values
-
 didt_min = Vmargin ./ L; % Maximum current slew rate for full sweep [A/s]
 
 % Creating a current frequency response plot
 f = [0:1e6];
-w = 2*pi*f;
-h_f = abs((Rt./L)./(1j*w'+(Rt./L))); %Each column cointain frequency
-                                      %response for a magnet [1]
-i_f = h_f.*Vmargin./Rt; % Current for each frequency [A]
-i_f_p = i_f./I;         % Ratio between max current and full scale
-                        % current [1]
-    
-loglog(f,i_f_p'*100);
+i_p_f = deflection_response(f', Rt, L, Vmargin, I);
+
+figure()
+loglog(f,i_p_f'*100);
 legend(sprintf('Standard corrector, L = %0.2g mH; I = %0.2g A; R=%0.2g Ohm', L(1)*1e3,I(1),R(1)), ...
        sprintf('45º corrector, L = %0.2g mH; I = %0.2g A; R = %0.2g Ohm', L(2)*1e3,I(2),R(2)));
 line([f_max f_max],ylim);
@@ -73,26 +60,64 @@ ylabel('Ratio to full deflection [%]')
 
 title(sprintf('Max current ratio vs frequency for R_{cb} = %0.2g Ohm, V_s = %0.2g V', R_cb, Vs));
 
-i_f_p(f_max,:)
-
 %% Calculating effect of cable length on response for both correctors
-
 Cb_length = [10:100]';
-R_cb = 2*Cb_length*Cb_res;
-Rt = R_cb + R + R_sh;
+R_cb_cl = 2*Cb_length*Cb_res;
+Rt_cl = R_cb_cl + R + R_sh;
 
-Vomax = Rt.*I;
+Vomax_cl = Rt_cl.*I;
 
-Vmargin_p = (Vs - Vswp) - (V_mid+Vomax/2);
-Vmargin_n = (V_mid - Vomax/2) - Vswn;     
-Vmargin = Vmargin_p + Vmargin_n;
+Vmargin_cl = Vswing - Vomax_cl;
 
-didt_min = Vmargin ./ L; % Maximum current slew rate for full sweep [A/s]
-    
-f_max = 10e3;
-w_max = 2*pi*f_max;
-h = abs((Rt./L)./(1j*w_max+(Rt./L))); %Each column cointain frequency
-                                      %response for a magnet [1]
-i = h.*Vmargin./Rt;  % Current for each frequency [A]
-i_p = i./I;         % Ratio between max current and full scale
-                     % current [1]
+didt_min_cl = Vmargin_cl ./ L; % Maximum current slew rate for full sweep [A/s]
+i_p_cl = deflection_response(f_max, Rt_cl, L, Vmargin_cl, I);
+
+figure()
+cl_df = subplot(2,2,1);
+plot(Cb_length,i_p_cl*100);
+legend('Standard magnet', '45^{\circ} magnet');
+title(sprintf('Max deflection dependence vs cable length (resistivity = %0.2g)', ...
+              Cb_res));
+xlabel('Cable length [m]');
+ylabel(sprintf('Max deflection at f = %0.2g [% of Full scale]',f_max));
+
+cl_sr = subplot(2,2,3)
+plot(Cb_length,didt_min_cl);
+ylabel('Maximum current slew rate [A/s]')
+xlabel('Cable length [m]')
+legend('Standard magnet', '45^{\circ} magnet');
+
+%% Plotting effect of increased Vs on response
+Vs_v = [3:0.1:5]';
+
+Vmid_s = (Vs_v - Vswp + Vswn)/2;
+Vswing_v = Vs_v - Vswp - Vswn;
+
+Vmargin_v = Vswing_v - Vomax;
+
+didt_min_v = Vmargin_v ./ L;
+i_p_v = deflection_response(f_max, Rt, L, Vmargin_v, I);
+
+vs_df = subplot(2,2,2);
+plot(Vs_v,i_p_v*100);
+legend('Standard magnet', '45^{\circ} magnet');
+title(sprintf('Max deflection dependence on supply voltage', ...
+              Cb_res));
+xlabel('OpAmp supply voltage [V]');
+ylabel(sprintf('Max deflection at f = %0.2g [% of Full scale]',f_max));
+
+vs_sr = subplot(2,2,4)
+plot(Vs_v,didt_min_v);
+ylabel('Maximum current slew rate [A/s]')
+xlabel('Supply voltage [V]')
+legend('Standard magnet', '45^{\circ} magnet');
+
+linkaxes([cl_df, cl_sr],'x');
+linkaxes([vs_df, vs_sr],'x')
+
+function i_p = deflection_response(f, R, L, V, I)
+    w = 2*pi*f;
+    H = abs((R./L)./(1j*w+(R./L)));
+    i = H.*V./R;
+    i_p = i./I;        
+end
