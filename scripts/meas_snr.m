582a0d8e49bfa4212fb227134054b4e1c132738a
Aylons <aylons@aylons.com.br>
2018-10-15 20:27:06 -0300

    Make it clearer by redefining RMS and peak values

--- scripts/meas_snr.m
+++ scripts/meas_snr.m
@@ -13,7 +13,7 @@ req_th_noise = 300e-12;     % Required beam deflection noise spectral density [r
 I_FS = 1;                   % Load current full-scale [A]
 th_FS = 30e-6;              % Beam deflection full-scale [rad]
 
-Rs = [1:50]*1e-3;
+Rs = [1:100]*1e-3;
 
 Gain = [ ...                   % Gain
     1; ...                  
@@ -30,15 +30,21 @@ rs_n = length(Rs);
 
 k_therm = 1.6568e-20;
 
+I_FS_rms = I_FS/sqrt(2); % RMS value for a sinusoidal signal with
+                         % full scale amplitude
+
 % ADC data (based on LTC2320-16 datasheet)
-SNR_adc = 80;               % SNR [dBFS]
+SNR_adc = 81;               % SNR [dBFS]
 fs = 1.2e6;                 % Sampling rate [Hz]
-FS_adc = 2/sqrt(2);                 % Full-scale voltage [V]
+FS_adc = 2;         % Full-scale voltage [V]
+FS_adc_rms = FS_adc/sqrt(2); % RMS value of a sinusoidal signal with
+                             % ADC's full scale  amplitude
 
 % ADC noise floor
-ADC_noisefloor = FS_adc*10^(-SNR_adc/20)/sqrt(fs/2);
+ADC_noisefloor = FS_adc_rms*10^(-SNR_adc/20)/sqrt(fs/2);
 
-Rs_power = Rs*(I_FS.^2);    %power dissipated in resistor
+Rs_power = Rs*(I_FS.^2);    %power dissipated in resistor (worst
+                            %case = DC value)
 
 total_noise = zeros(gain_n,rs_n);
 ADC_signal = zeros(gain_n, rs_n);
@@ -50,11 +56,11 @@ for i = 1:gain_n
     G = Gain(i);
     noise = ad8429_noise(G,Rs);
     imeas_noise = 1./Rs./G.*noise;
-    FS_noise = th_FS./I_FS.*imeas_noise;
+    FS_noise = th_FS./I_FS_rms.*imeas_noise;
 
-    ADC_signal(i,:) = G*Rs*I_FS;
-    ADC_signal(i,ADC_signal(i,:)>FS_adc) = NaN;
-    Fp_ADC(i,:) = ADC_signal(i,:)./FS_adc; %proportion of signal to ADC full
+    ADC_signal(i,:) = G*Rs*I_FS_rms;
+    ADC_signal(i,ADC_signal(i,:)>FS_adc_rms) = NaN;
+    Fp_ADC(i,:) = ADC_signal(i,:)./FS_adc_rms; %proportion of signal to ADC full
                                 %scale
 
     total_noise(i,:) = sqrt(ADC_noisefloor.^2+noise.^2)*sqrt(fs/2);
